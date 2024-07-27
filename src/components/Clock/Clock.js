24498d0c53b31248decbc2260ff617b684201103
Allovallo <grossco@meta.ua>
2024-07-27 18:16:47 +0300

    Clock updated

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,40 +1,55 @@
+import { useState } from 'react';
 import styles from './Clock.module.css';
-import React, { Component } from 'react';
 
-export default class OldClock extends Component {
-  state = {
-    time: new Date(),
-  };
-
-  intervalId = null;
-
-  componentDidMount() {
-    this.intervalId = setInterval(() => {
-      console.log('Це інтервал кожні 1000ms ' + Date.now());
-      this.setState({ time: new Date() });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    console.log('Цей метод визивається перед розмонтуванням компоненту');
-    this.stop();
-  }
-
-  stop = () => {
-    clearInterval(this.intervalId);
-  };
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <p className={styles.clockface}>
-          Поточний час:
-          {this.state.time.toLocaleTimeString()}
-        </p>
-        <button type="button" onClick={this.stop}>
-          Зупинити
-        </button>
-      </div>
-    );
-  }
+export default function Clock() {
+  const [time, setTime] = useState(() => new Date());
+  return (
+    <div className={styles.container}>
+      <p className={styles.clockface}>
+        Поточний час:
+        {this.state.time.toLocaleTimeString()}
+      </p>
+      <button type="button" onClick={this.stop}>
+        Зупинити
+      </button>
+    </div>
+  );
 }
+
+// class OldClock extends Component {
+//   state = {
+//     time: new Date(),
+//   };
+
+//   intervalId = null;
+
+//   componentDidMount() {
+//     this.intervalId = setInterval(() => {
+//       console.log('Це інтервал кожні 1000ms ' + Date.now());
+//       this.setState({ time: new Date() });
+//     }, 1000);
+//   }
+
+//   componentWillUnmount() {
+//     console.log('Цей метод визивається перед розмонтуванням компоненту');
+//     this.stop();
+//   }
+
+//   stop = () => {
+//     clearInterval(this.intervalId);
+//   };
+
+//   render() {
+//     return (
+//       <div className={styles.container}>
+//         <p className={styles.clockface}>
+//           Поточний час:
+//           {this.state.time.toLocaleTimeString()}
+//         </p>
+//         <button type="button" onClick={this.stop}>
+//           Зупинити
+//         </button>
+//       </div>
+//     );
+//   }
+// }
